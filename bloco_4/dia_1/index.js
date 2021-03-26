1e0e36d651a7e4882cfd96e73516745520d897cf
Bernardo <bernardocezario21@gmail.com>
2021-03-25 21:36:03 -0300

    Finish step 3 from exercise

---
+++ bloco_4/dia_1/index.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 15;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
+
+if (a > b) {
+  console.log("a é maior que b");
+} else {
+  console.log("b é maior que a");
+}
