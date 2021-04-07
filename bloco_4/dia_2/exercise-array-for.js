1ccd391e87b918fa44be0d05a1b88bba02ac27d4
Bernardo Coelho <bernardocezario21@gmail.com>
2021-04-07 19:32:09 -0300

    Add chhanges from git clone

---
+++ bloco_4/dia_2/exercise-array-for.js
@@ -0,0 +1,42 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+let sum = 0
+let higherNumber = 0;
+let cousinNumber = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+}
+
+for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+  result += numbers[index2];
+}
+console.log(result);
+
+let average = result / numbers.length;
+
+console.log(average);
+
+if (average > 20) {
+  console.log("Valor maior que 20")
+} else {
+  console.log("Valor menor ou igual a 20")
+}
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
+}
+console.log(higherNumber);
+
+for (let index3 = 0; index3 < numbers.length; index3 += 1) {
+  if (numbers[index3] % 2 !== 0){
+    result += 1;
+  }
+}
+if (result === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(result);
+}
